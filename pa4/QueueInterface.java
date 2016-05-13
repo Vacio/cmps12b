54033c016e2a717086fe223c59c770b8bcb87ad9
PhaseIso <PhaseIso@users.noreply.github.com>
2016-05-13 14:15:04 -0700

    Update QueueInterface.java

--- pa4/QueueInterface.java
+++ pa4/QueueInterface.java
@@ -1,34 +1,34 @@
 // QueueInterface.java
 public interface QueueInterface{
 
-   // isEmpty()
-   // pre: none
-   // post: returns true if this Queue is empty, false otherwise
+   /* isEmpty()
+    pre: none
+    post: returns true if this Queue is empty, false otherwise*/
    public boolean isEmpty();
-   // length()
-   // pre: none
-   // post: returns the length of this Queue.
+   /* length()
+    pre: none
+    post: returns the length of this Queue.*/
    public int length();
-   // enqueue()
-   // adds newItem to back of this Queue
-   // pre: none
-   // post: !isEmpty()
+   /* enqueue()
+    adds newItem to back of this Queue
+    pre: none
+    post: !isEmpty()*/
    public void enqueue(Object newItem);
-   // dequeue()
-   // deletes and returns item from front of this Queue
-   // pre: !isEmpty()
-   // post: this Queue will have one fewer element
+   /* dequeue()
+    deletes and returns item from front of this Queue
+    pre: !isEmpty()
+    post: this Queue will have one fewer element*/
    public Object dequeue() throws QueueEmptyException;
-   // peek()
-   // pre: !isEmpty()
-   // post: returns item at front of Queue
+   /* peek()
+    pre: !isEmpty()
+    post: returns item at front of Queue*/
    public Object peek() throws QueueEmptyException;
-   // dequeueAll()
+   /* dequeueAll()
    // sets this Queue to the empty state
    // pre: !isEmpty()
-   // post: isEmpty()
+   // post: isEmpty()*/
    public void dequeueAll() throws QueueEmptyException;
-   // toString()
-   // overrides Object's toString() method 
+   /* toString()
+    overrides Object's toString() method */
   public String toString();
 }
