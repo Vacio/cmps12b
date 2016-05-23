041c0804e123e3d89b4a5e37d136ab86c32dc8fc
PhaseIso <bensherriff@yahoo.com>
2016-05-23 14:41:44 -0700

    Update

--- pa5/Dictionary.c
+++ pa5/Dictionary.c
@@ -2,20 +2,3 @@
 #include <stdio.h>
 #include "Dictionary."
 
-int main(int argc, char* argv[]) {
-	/* Variable Initialization */
-	//int
-	FILE* in;
-	FILE* out;
-	DictionaryRef D = new Dictionary();
-
-	/* Check command line arguments */
-	if (argc != 3) {
-		prtinf("Usage: %s infile outfile\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Open files */
-	in = fopen(argv[1], "r");
-	out = fopen(argv[2], "w");
-}
