ab441658daac8b2463e6f454999bb2b2a161e142
PhaseIso <PhaseIso@users.noreply.github.com>
2016-06-05 00:07:01 -0700

    Create problem10.c
    Not complete

---
+++ Examples/Practice/Final/problem10.c
@@ -0,0 +1,21 @@
+/* Given the NodeObj structure and Node reerence from problem 9 above, write the following C functions:
+ A. A constructor that returns a reference to a new NodeObj allocated from heap memory with its item
+    field set to x and its next field set to NULL
+ B. A destructor that frees the heap memory associated with a Node, and sets its reference to NULL
+ C. A recursive function with heading void clearList(Node H) that frees all heap memory associated
+    with a linked list headed by H */
+
+// A
+Node new Node(int x) {
+
+}
+
+// B
+void freeNode(Node* pN) {
+
+}
+
+// C
+void clearList(Node H) {
+
+}
