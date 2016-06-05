ef4f4e212f462daef8d047962ee796146a31522e
PhaseIso <PhaseIso@users.noreply.github.com>
2016-06-05 16:41:18 -0700

    Update problem5.java

--- Examples/Practice/Final/problem5.java
+++ Examples/Practice/Final/problem5.java
@@ -1,16 +1,23 @@
 /* Write a Java function with the heading void sortWords(String[] W) that sorts is array argument W
 in alphabetical order. Do this by implementing the Insertion Sort algorithm discussed in class. */
 
+public static void main(String[] args) {
+  String array[] = {"G", "F", "D", "B", "C", "E", "A"};
+  for(int i = 0; i < array.length(); i++) {
+    System.out.println(sortWords(array[i] + " ");
+  }
+}
+
 void sortWords(String[] W) {
   int i, j;
   String temp;
-  for(j=1; j<W.length(); j++) {
-    temp = W[j];
-    i = j-1;
-    while(i >= 0 && temp.compareTo(W[i]) < 0) {
-      W[i+1] = W[i];
-      i--;
+  for(int i = 0; i < W.length(); i++) {
+    for(int j = i+1; J < W.length(); j++) {
+      if (W[i].compareTo(W[j]) > 0) {
+        temp = W[i];
+        W[i] = W[j];
+        W[j] = temp;
+      }
     }
-    W[i+1] = temp;
   }
 }
